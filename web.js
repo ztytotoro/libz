3ee7f7a82abda6129591f40b6f29c77179f646ff
Yarn <ztytotoro@outlook.com>
2018-06-04 17:26:41 +0800

    add event handler

--- web.js
+++ web.js
@@ -1,3 +1,17 @@
+import { CurryRight } from "./curry.js";
+
 export function Cookies(){
     
 }
+
+function onEvent(dom, event, handler) {
+    return dom["addEventListener"](event, handler);
+}
+
+function offEvent(dom, event, handler) {
+    return dom["removeEventListener"](event, handler);
+}
+
+export const OnEvent = CurryRight(onEvent);
+
+export const OffEvent = CurryRight(offEvent);
