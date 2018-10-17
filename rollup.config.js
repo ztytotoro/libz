8469f757eb321b3397ed8207de9ed85c016fae79
Yarn <ztytotoro@outlook.com>
2018-10-17 16:15:17 +0800

    migrate to typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
 // rollup.config.js (building more than one bundle)
+import typescript from 'rollup-plugin-typescript';
+
 export default [{
-    input: 'index.js',
+    input: 'src/main.ts',
     output: {
       file: 'dist/libz.js',
       format: 'es',
       name: "libz"
-    }
+    },
+    plugins: [
+      typescript({lib: ["es5", "es6", "dom"], target: "es5", experimentalDecorators: true})
+    ]
   }];
